c01666a72fe25470fd2836335e732eee68e4c292
Ethan Isola <isolaethan@gmail.com>
2022-02-04 22:55:48 -0600

    Done.

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,26 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+function ItemForm({onItemFormSubmit}) {
+  const [itemName, setItemName] = useState("")
+  const [newCategory, setCategory] = useState("Produce")
+  
+  const handleNameChange = ({target})=>{
+    setItemName(target.value)
+  }
+
+  const handleCategoryChange = ({target})=>{
+    setCategory(target.value)
+  }
+  const newItem = {
+    id: uuid(), // the `uuid` library can be used to generate a unique id
+    name: itemName,
+    category: newCategory,
+  };
+
+  const onSubmit=()=>{
+    return onItemFormSubmit(newItem)
+    console.log("hi")
+  }
+
+  const prevDefault = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit = {prevDefault}>
+      
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange = {handleNameChange} value = {itemName} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </label>
 
-      <button type="submit">Add to List</button>
+      <button type="submit" onClick = {onSubmit} onSubmit={onSubmit}>Add to List</button>
     </form>
   );
 }
 
 export default ItemForm;
+
+
+
+
+
+
+
+
+
+
+// function ItemForm({}) {
+
+//   const newItem = {
+//     id: uuid(), // the `uuid` library can be used to generate a unique id
+//     name: newItemName,
+//     category: formCategory,
+//   };
+  
+
+//   return (
+//     <form className="NewItem" onSubmit = {prevSubmit}>
+      
+//       <label>
+//         Name:
+//         <input type="text" name="name" onChange = {onNewName} value = {newItemName}/>
+//       </label>
+
+//       <label>
+//         Category:
+//         <select name="category" onChange={onItemCategoryChange}>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </label>
+
+//       <button type="submit" onSubmit={onItemFormSubmit(newItem)}>Add to List</button>
+//     </form>
+//   );
+// }
+
+// export default ItemForm;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,25 +3,51 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
+
+
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  
+  const [searchText, setSearchText] = useState("")
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
+  const [finalList, setList] = useState(items)
+
+
+
+
+  const handleSearch = ({target}) => {
+    setSearchText(target.value)
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  const handleCategoryNameChange = ({target}) => {
+    setSelectedCategory(target.value);
+  }
+
+  const onItemFormSubmit = (newItem) => {
+    return setList([...items, newItem])
+    console.log(finalList)
+  }
+  
+
+  const itemsToDisplay = finalList.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
+  
+
+  let searchResults = itemsToDisplay.filter((item) => {
+      return(
+        item.name.toLowerCase().includes(searchText.toLowerCase())
+      )
+  })
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit = {onItemFormSubmit} />
+      <Filter onCategoryChange={handleCategoryNameChange} onSearchChange ={handleSearch} search = {searchText}/>
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+        {searchResults.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
